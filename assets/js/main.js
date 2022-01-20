23dcff7d89b57e5dd6b43d69ac23af0768864b02
Nguyễn Văn Quốc Hùng <sine.hungnguyen@gmail.com>
2022-01-21 01:07:50 +0700

    fix subnav bug, add event click outside nav to close nav and subnav

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ const currentHeight = header.clientHeight;
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector(".modal-close");
 const buyTicketsBtns = document.querySelectorAll(".js-buy-ticket");
-var isDisplay = true;
+const menuItems = document.querySelectorAll("#nav li a");
 
 // Open/Close mobile menu
 mobileMenu.onclick = function () {
@@ -14,30 +14,38 @@ mobileMenu.onclick = function () {
         header.style.height = "auto";
         header.style.overflow = "visible";
     } else {
+        if (subnav.classList.contains("subnav-open"))
+            subnav.classList.remove("subnav-open");
         header.style.height = null;
         header.style.overflow = "hidden";
+        document.getElementById("nav-more-item").classList.remove("nav-hover");
     }
 };
 
 // Auto close menu on click link
-const menuItems = document.querySelectorAll("#nav li a");
-
 menuItems.forEach(function (menuItem) {
     menuItem.onclick = function () {
         if (
             menuItem.nextElementSibling &&
             menuItem.nextElementSibling.classList.contains("subnav")
         ) {
-            if (!isDisplay) {
-                subnav.style.display = "none";
+            if (subnav.classList.contains("subnav-open")) {
+                subnav.classList.remove("subnav-open");
+                menuItem.classList.remove("nav-hover");
             } else {
-                subnav.style.display = "block";
+                menuItem.classList.add("nav-hover");
+                header.style.overflow = "visible";
+                subnav.classList.add("subnav-open");
             }
-            isDisplay = !isDisplay;
             event.preventDefault();
         } else {
+            if (subnav.classList.contains("subnav-open"))
+                subnav.classList.remove("subnav-open");
             header.style.overflow = "hidden";
             header.style.height = null;
+            document
+                .getElementById("nav-more-item")
+                .classList.remove("nav-hover");
         }
     };
 });
@@ -54,9 +62,19 @@ modalClose.addEventListener("click", function () {
     modal.classList.remove("modal-open");
 });
 
-// Click outside modal container
+// Click outside modal container to close
 modal.addEventListener("click", function (e) {
     if (!e.target.closest(".modal-container")) {
         modal.classList.remove("modal-open");
+    }
+});
+
+// Click outsite sub-navigation to close
+document.addEventListener("click", function (e) {
+    if (!e.target.closest("#nav") && !e.target.closest("#mobile-menu")) {
+        subnav.classList.remove("subnav-open");
+        document.getElementById("nav-more-item").classList.remove("nav-hover");
+        header.style.overflow = "hidden";
+        header.style.height = null;
     }
 });
